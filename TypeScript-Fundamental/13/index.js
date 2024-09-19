6c667ffbd5c2eaf5d30d36997e44b3788f71520c
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-09-19 11:08:43 +0430

    extending type aliases

---
+++ TypeScript-Fundamental/13/index.js
@@ -0,0 +1,22 @@
+"use strict";
+//------------------------
+// extending type aliases
+//------------------------
+const personOne = {
+    id: 1,
+    firstName: "mario",
+};
+const personTwo = {
+    id: "2",
+    firstName: "yoshi",
+    email: "yoshi@netninja.dev",
+};
+const personThree = {
+    email: "peach@netninja.dev",
+};
+function printUser(user) {
+    console.log(user.id, user.email, user.firstName);
+}
+// printUser(personOne) --> not allowed
+printUser(personTwo);
+// printUser(personThree) --> not allowed
