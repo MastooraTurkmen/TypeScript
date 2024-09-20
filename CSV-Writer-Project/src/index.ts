72144bf53b0d38ce824c39e7166fdb5194152d43
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-09-20 09:33:21 +0430

    test csv

---
+++ CSV-Writer-Project/src/index.ts
@@ -0,0 +1,56 @@
+//-------------------
+// CSV Writer Project
+//-------------------
+
+import { appendFileSync } from "fs";
+
+interface Payment {
+  id: number;
+  amount: number;
+  to: string;
+  notes: string;
+}
+
+type PaymentColumns = ("id" | "amount" | "to" | "notes")[];
+
+class CSVWriter {
+  constructor(private columns: PaymentColumns) {
+    this.csv = this.columns.join(",") + "\n";
+  }
+  private csv: string;
+
+  save(filename: string): void {
+    appendFileSync(filename, this.csv);
+    this.csv = "\n";
+    console.log("file save to", filename);
+  }
+
+  addRows(values: Payment[]): void {
+    let rows = values.map((v) => this.formatRow(v));
+    this.csv += rows.join("\n");
+    console.log(this.csv);
+  }
+
+  private formatRow(p: Payment): string {
+    return this.columns.map((col) => p[col]).join(",");
+  }
+}
+
+const writer = new CSVWriter(["id", "amount", "to", "notes"]);
+
+writer.addRows([
+  {
+    id: 1,
+    amount: 50,
+    to: "yoshi",
+    notes: "for design work",
+  },
+  {
+    id: 2,
+    amount: 100,
+    to: "mario",
+    notes: "for DEV work",
+  },
+]);
+
+writer.save('./data/payments.csv')
