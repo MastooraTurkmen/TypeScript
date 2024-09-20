e68297dc2672e7d039e826324fe85e98087d1528
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-09-20 09:33:16 +0430

    CSV Writer Project

---
+++ CSV-Writer-Project/dist/index.js
@@ -0,0 +1,41 @@
+"use strict";
+//-------------------
+// CSV Writer Project
+//-------------------
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs_1 = require("fs");
+class CSVWriter {
+    constructor(columns) {
+        this.columns = columns;
+        this.csv = this.columns.join(",") + "\n";
+    }
+    save(filename) {
+        (0, fs_1.appendFileSync)(filename, this.csv);
+        this.csv = "\n";
+        console.log("file save to", filename);
+    }
+    addRows(values) {
+        let rows = values.map((v) => this.formatRow(v));
+        this.csv += rows.join("\n");
+        console.log(this.csv);
+    }
+    formatRow(p) {
+        return this.columns.map((col) => p[col]).join(",");
+    }
+}
+const writer = new CSVWriter(["id", "amount", "to", "notes"]);
+writer.addRows([
+    {
+        id: 1,
+        amount: 50,
+        to: "yoshi",
+        notes: "for design work",
+    },
+    {
+        id: 2,
+        amount: 100,
+        to: "mario",
+        notes: "for DEV work",
+    },
+]);
+writer.save('./data/payments.csv');
