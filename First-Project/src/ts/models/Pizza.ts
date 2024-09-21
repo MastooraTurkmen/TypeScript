6a34817298689af7bcb0c92e8a88a811cd3aa718
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-09-21 16:45:46 +0430

    Pizza Palace Menu

---
+++ First-Project/src/ts/models/Pizza.ts
@@ -0,0 +1,12 @@
+import { DataResource } from "../services/DataResource";
+
+export interface PizzaProps {
+  title: string;
+  description: string;
+  toppings: string[];
+  price: number;
+}
+
+export const Pizza = new DataResource<PizzaProps>(
+  "http://localhost:3000/pizzas"
+);
