130c8591be8b4596d7f258a29b8d9e2cf7ca4c7f
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-09-21 21:02:10 +0430

    Enums

---
+++ TypeScript-Fundamental/20-Enums/index.ts
@@ -0,0 +1,26 @@
+// ---------
+// Enums 101
+// ---------
+
+enum Priority {
+  Lowest = 0,
+  Low = 1,
+  Medium = 2,
+  High = 3,
+  Urgent = 4,
+}
+
+function addTicket(dtails: string, priority: number) {
+  if (priority === Priority.Lowest) {
+  }
+  if (priority === 1) {
+  }
+  if (priority === 2) {
+  }
+  if (priority === 3) {
+  }
+  if (priority === 4) {
+  }
+}
+
+addTicket("fix computer", Priority.Urgent);
