6de0a79c86e1abd1cfa4fb95e7c76769f2193a35
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-09-21 18:48:43 +0430

    next with typescript simple project

---
+++ ninja-blog/src/app/about/page.tsx
@@ -0,0 +1,25 @@
+export default function About() {
+  return (
+    <main>
+      <h1>About page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum iusto
+        alias amet enim voluptates aliquid nesciunt, provident dolorum laborum
+        quod.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempora labore
+        asperiores pariatur! Delectus quam accusantium magnam exercitationem
+        facilis sit. Quod. Lorem ipsum dolor sit, amet consectetur adipisicing
+        elit. Doloremque corporis beatae corrupti ratione a eius cumque
+        veritatis vitae reprehenderit ex? Lorem, ipsum dolor sit amet
+        consectetur adipisicing elit. Blanditiis, unde? Lorem ipsum dolor sit
+        amet consectetur adipisicing elit. Vel, suscipit! Lorem ipsum dolor sit
+        amet consectetur adipisicing elit. Reprehenderit, repellat. Lorem ipsum
+        dolor sit amet consectetur adipisicing elit. Dicta tempore veniam sunt
+        totam officiis necessitatibus at iusto eos provident,labore sit
+        molestias qui nam nisi harum eligendi quisquam aliquam incidunt?
+      </p>
+    </main>
+  );
+}
